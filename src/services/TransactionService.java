379fcc1f6924cea65b90620d6f45d9c11a0dece9
JoelSRJE <JoelSRJE@hotmail.com>
2025-11-03 13:41:29 +0100

    TransactionService & ITransactionService

--- src/services/TransactionService.java
+++ src/services/TransactionService.java
@@ -1,4 +1,48 @@
 package services;
 
-public class TransactionService {
+import models.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
+public class TransactionService implements ITransactionService {
+    @Override
+    public void addTransaction(Transaction transaction) throws Exception {
+        System.out.println("Added Transaction");
+    }
+
+    @Override
+    public Transaction deleteTransaction(UUID transactionId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public double showBalance() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public double getAllIncomes() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public double getAllExpenses() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<Transaction> displayExpenses() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public List<Transaction> displayIncomes() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public List<Transaction> showAllTransactions() throws Exception {
+        return List.of();
+    }
 }
