c114b489a1383f4f01682844f2bfdb759a20be6f
JoelSRJE <JoelSRJE@hotmail.com>
2025-11-08 22:06:09 +0100

    Some tests for practice

--- src/test/java/main/services/AccountServiceTest.java
+++ src/test/java/main/services/AccountServiceTest.java
@@ -6,12 +6,14 @@ import main.repositories.IApplicationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//Some tests for practice
 class AccountServiceTest {
 
     private IApplicationRepository dummyRepository;
@@ -41,7 +43,7 @@ class AccountServiceTest {
 
         @Override
         public void deleteAccount(String accountName) {
-
+            savedAccounts.removeIf(account -> account.getAccountName().equals(accountName));
         }
     }
 
@@ -86,7 +88,134 @@ class AccountServiceTest {
     }
 
     @Test
+    void testDeleteAccount_success() throws Exception {
+        Account account1 = accountService.createAccount("Test account1");
+        UUID accountId1 = account1.getAccountID();
+
+        Account account2 = accountService.createAccount("Test account2");
+        UUID accountId2 = account2.getAccountID();
+
+        assertEquals(2, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test account1", accountService.getAccount(accountId1).getAccountName());
+        assertEquals("Test account2", accountService.getAccount(accountId2).getAccountName());
+
+        accountService.deleteAccount(accountId1);
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+
+        assertThrows(Exception.class, () -> accountService.getAccount(accountId1));
+
+        assertEquals("Test account2", accountService.getAccount(accountId2).getAccountName());
+    }
+
+    @Test
     void testAddTransaction_success() throws Exception {
-        // Todo
+        Account account = accountService.createAccount("Test Account");
+        UUID accountId = account.getAccountID();
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test Account", accountService.getAccount(accountId).getAccountName());
+
+        Transaction testTransaction = new Transaction("Salary", 22000, LocalDate.of(2025, 11, 8), true);
+
+        accountService.addTransaction(accountId, testTransaction);
+
+        List<Transaction> transactions = accountService.getAllTransactions(accountId);
+
+        assertTrue(transactions.contains(testTransaction));
+    }
+
+    @Test
+    void testAddTransaction_failure() throws Exception {
+        Account account = accountService.createAccount("Test account");
+        UUID accountId = account.getAccountID();
+        UUID wrongAccountId = UUID.randomUUID();
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test account", accountService.getAccount(accountId).getAccountName());
+
+        Transaction testTransaction = new Transaction("Swish", 500, LocalDate.of(2025, 11, 8), true);
+
+        accountService.addTransaction(accountId, testTransaction);
+
+        List<Transaction> transactions = accountService.getAllTransactions(accountId);
+
+        assertTrue(transactions.contains(testTransaction));
+
+        assertThrows(Exception.class, () -> accountService.getAllTransactions(wrongAccountId));
+    }
+
+    @Test
+    void testRemoveTransaction_success() throws Exception {
+        Account account = accountService.createAccount("Test account");
+        UUID accountId = account.getAccountID();
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test account", accountService.getAccount(accountId).getAccountName());
+
+        List<Transaction> transactionsBefore = accountService.getAllTransactions(accountId);
+        int before = transactionsBefore.size();
+
+        Transaction testTransaction = new Transaction("Salary", 22000, LocalDate.of(2025, 10, 25), true);
+        accountService.addTransaction(accountId, testTransaction);
+
+        List<Transaction> transactionsAfter = accountService.getAllTransactions(accountId);
+        assertEquals(before + 1, transactionsAfter.size());
+
+        UUID transactionId = transactionsAfter.get(transactionsAfter.size() - 1).getId();
+        accountService.removeTransaction(accountId, transactionId);
+
+        List<Transaction> result = accountService.getAllTransactions(accountId);
+        assertEquals(before, result.size());
+    }
+
+    @Test
+    void testRemoveTransaction_failure() throws Exception {
+        Account account = accountService.createAccount("Test account");
+        UUID accountId = account.getAccountID();
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test account", accountService.getAccount(accountId).getAccountName());
+
+        UUID wrongTransactionId = UUID.randomUUID();
+
+        List<Transaction> transactionsBefore = accountService.getAllTransactions(accountId);
+        int listBefore = transactionsBefore.size();
+
+        Transaction testTransaction = new Transaction("Salary", 22000, LocalDate.of(2025, 10, 25), true);
+        accountService.addTransaction(accountId, testTransaction);
+
+        List<Transaction> transactionsAfter = accountService.getAllTransactions(accountId);
+        int listAfter = transactionsAfter.size();
+
+        assertEquals(listBefore + 1, listAfter);
+
+        assertThrows(Exception.class, () -> accountService.removeTransaction(accountId, wrongTransactionId));
+    }
+
+    @Test
+    void testShowBalanceAfterTransaction_success() throws Exception {
+        Account account = accountService.createAccount("Test account");
+        UUID accountId = account.getAccountID();
+
+        assertEquals(1, ((DummyRepository) dummyRepository).savedAccounts.size());
+        assertEquals("Test account", accountService.getAccount(accountId).getAccountName());
+
+        List<Transaction> transactions = accountService.getAllTransactions(accountId);
+        int transactionsBefore = transactions.size();
+
+        double previousBalance = accountService.getBalance(accountId);
+
+        Transaction testTransaction = new Transaction("Test", 100, LocalDate.of(2025, 11, 8), true);
+        accountService.addTransaction(accountId, testTransaction);
+
+        List<Transaction> transactions2 = accountService.getAllTransactions(accountId);
+        int transactionsAfter = transactions2.size();
+
+        assertEquals(transactionsBefore + 1, transactionsAfter);
+
+        double newBalance = accountService.getBalance(accountId);
+
+        assertEquals(previousBalance + 100, newBalance);
     }
 }
